e65aeb5c43c8579078f3e296221903e0b5f03040
JenniferTantely <hei.jennifer.2@gmail.com>
2024-08-07 12:38:00 +0300

    feat: add vue folder

---
+++ vue/src/App.jsx
@@ -0,0 +1,19 @@
+import React, {useEffect} from "react";
+
+// class AppCLazz extends React.Component {
+//   constructor (){
+//     super();
+//   }
+
+// } un truc que tu dois savoir
+
+
+
+function App() {
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
---
+++ vue/src/components/Testes7.jsx
@@ -0,0 +1,8 @@
+//rafce magicall
+const Testes7 = () => {
+  return (
+    <div>Testes7</div>
+  )
+}
+
+export default Testes7
