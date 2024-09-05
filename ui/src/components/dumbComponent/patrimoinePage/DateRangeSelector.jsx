e492f54566452271cc9534db8bc3cf4bfd5c1360
JenniferTantely <hei.jennifer.2@gmail.com>
2024-09-05 17:35:31 +0300

    fix: date range selector

--- ui/src/components/dumbComponent/patrimoinePage/DateRangeSelector.jsx
+++ ui/src/components/dumbComponent/patrimoinePage/DateRangeSelector.jsx
@@ -3,7 +3,7 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import '../css/patrimoine.css'
 
-const array = [1, 2, 3, 4, 5];
+const array = [1, 2, 3, 4, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
 function DateRangeSelector({
   dateDebut,
